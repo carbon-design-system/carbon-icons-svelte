c53bcde10cb85c82ad89df209b65b5166ae1b80a
Eric Liu <ericyl.us@gmail.com>
2020-07-24 18:34:42 -0700

    chore(examples): add typescript set-up

---
+++ examples/rollup-typescript/src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Add16 from "carbon-icons-svelte/lib/Add16";
+</script>
+
+<Add16 title="hello" />
+
+<!-- Try typing "<Dele..." -->
+<!-- With the VSCode extension, it should autocomplete the "Delete16" import-->
