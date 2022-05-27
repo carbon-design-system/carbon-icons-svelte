ba231a5714dbafa36e52f0781522090beed02f7e
metonym <ericyl.us@gmail.com>
2022-05-27 09:29:46 -0700

    feat: upgrade `@carbon/icons` to v11.3.0 (#146)
    * feat: upgrade `@carbon/icons` to v11.3.0
    
    * chore(preview): upgrade deps

--- tests/import.test.ts
+++ tests/import.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from "vitest";
 import * as icons from "../lib";
 
 test("imports", () => {
-  expect(Object.keys(icons).length).toEqual(1922);
+  expect(Object.keys(icons).length).toEqual(1938);
 });
