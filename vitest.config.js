92b8f9014adbff2ee74ef4fcc608ea7e1366a46e
Eric Liu <ericyl.us@gmail.com>
2022-07-22 10:15:50 -0700

    chore(deps-dev): use vitest instead ts-node-dev as runner

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-export default defineConfig({
+/** @type {import('vite').UserConfig} */
+export default {
   plugins: [
     svelte({
-      hot: !process.env.VITEST,
+      hot: false,
     }),
   ],
-});
+};
