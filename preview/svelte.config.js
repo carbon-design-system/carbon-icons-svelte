7ad5d141f507f1472f7d1c1f4291fef67c3e6699
metonym <ericyl.us@gmail.com>
2022-09-16 12:23:37 -0700

    chore(preview): set immutable: true

--- preview/svelte.config.js
+++ preview/svelte.config.js
@@ -3,6 +3,9 @@ import { optimizeImports } from "carbon-preprocess-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  compilerOptions: {
+    immutable: true,
+  },
   preprocess: [optimizeImports()],
   kit: {
     adapter: adapter(),
