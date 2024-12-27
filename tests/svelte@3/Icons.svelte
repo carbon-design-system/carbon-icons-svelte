222913a141031f363a8c5385289f359b2f9a04eb
Eric Liu <ericyl.us@gmail.com>
2024-12-27 12:09:55 -0800

    test: assert types for Svelte 3, 4, 5

---
+++ tests/svelte@3/Icons.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import {
+    Add,
+    Assembly,
+    Beta,
+    BookmarkAdd,
+    BreakingChange,
+    LogoSvelte,
+    Plan,
+    ZSystems,
+    BatchJob,
+  } from "carbon-icons-svelte";
+  import type { CarbonIconProps } from "carbon-icons-svelte";
+  import Icon from "carbon-icons-svelte/lib/Accessibility.svelte";
+
+  const props: CarbonIconProps = {
+    size: 32,
+    fill: "red",
+  };
+
+  const icon = new Icon({ target: document.body, props });
+
+  $: console.log(icon.$$prop_def);
+  $: console.log(typeof Assembly);
+</script>
+
+<Add size={20} fill="red" title="title" data-test="id" />
+<Icon style={null} />
+<BookmarkAdd />
+<BreakingChange />
+<LogoSvelte />
+<Plan />
+<ZSystems />
+<BatchJob />
+<svelte:component this={Beta} />
