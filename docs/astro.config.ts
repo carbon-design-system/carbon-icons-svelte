61885d29b490714e08af5d727eb2ec8dacaa18ab
Eric Liu <ericyl.us@gmail.com>
2025-11-22 13:43:31 -0800

    docs: use `carbon-preprocess-svelte` (#217)

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,10 +1,16 @@
 import svelte from "@astrojs/svelte";
 import { defineConfig } from "astro/config";
+import { optimizeCss, optimizeImports } from "carbon-preprocess-svelte";
 import { version } from "../package.json" assert { type: "json" };
 
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [
+    svelte({
+      preprocess: [optimizeImports()],
+    }),
+  ],
   vite: {
+    plugins: [optimizeCss()],
     define: {
       __VERSION: JSON.stringify(version),
     },
