f70597b71b822466dd614b10b3d1519d28face77
Eric Liu <ericyl.us@gmail.com>
2026-06-05 13:56:10 -0700

    feat(deps-dev): upgrade `@carbon/icons` 11.80 -> 11.81 (#226)

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,7 +5,7 @@ import { template } from "../src/template";
 
 test("imports", async () => {
   const icons = await buildIcons();
-  expect(icons.length).toEqual(2681);
+  expect(icons.length).toEqual(2687);
   expect(icons).toMatchSnapshot();
 });
 
